068d7e4c36a501ea0665ed817af39309c265a852
Nate Rudnick Aldridge <nr265@cornell.edu>
2017-03-21 01:06:10 +0000

    Added more caps

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#### NOTES:
+### =>  Brand can be brand or subsidiary
+###
+#####
 
 #####
 # Bottlecap.create(drink: "", brand: "", parent: "", favorite: false, cap_detail_list: "", pinned: false)
@@ -12,28 +9,28 @@
 
 Bottlecap.create(drink: "Am I Right or Amarillo", brand: "Bell's Brewery", parent: "Bell's Brewery", favorite: false, cap_detail_list: "orange, cowboy hat", pinned: false)
 Bottlecap.create(drink: "Amber Ale", brand: "Bell's Brewery", parent: "Bell's Brewery", favorite: false, cap_detail_list: "red, maroon, heron, bird", pinned: false)
-Bottlecap.create(drink: "Pale Ale", brand: "Omission Beer", parent: "Omission Beer", favorite: false, cap_detail_list: "blue, black, O", pinned: false)
-Bottlecap.create(drink: "Sangria Soda", brand: "Topo Chico", parent: "Topo Chico", favorite: false, cap_detail_list: "purple, grapes", pinned: false)
+Bottlecap.create(drink: "Pale Ale", brand: "Omission Beer", parent: "Omission Beer", favorite: false, cap_detail_list: "blue, black, big letter", pinned: false)
+Bottlecap.create(drink: "Sangria Soda", brand: "Topo Chico", parent: "Topo Chico", favorite: false, cap_detail_list: "purple, silver, grapes", pinned: false)
 Bottlecap.create(drink: "Palma Louca", brand: "Cerveja Brasil", parent: "Cerveja Brasil", favorite: false, cap_detail_list: "blue, gold", pinned: false)
 Bottlecap.create(drink: "Pineapple Soda", brand: "Fanta", parent: "Coca-Cola", favorite: false, cap_detail_list: "yellow", pinned: false)
 Bottlecap.create(drink: "Virgil's Root Beer", brand: "Virgil's", parent: "Reed's", favorite: false, cap_detail_list: "white, gold, red", pinned: false)
-Bottlecap.create(drink: "Summer Shandy", brand: "Jacob Leinenkugel Brewing Company", parent: "SABMiller", favorite: false, cap_detail_list: "light blue, lemon", pinned: false)
+Bottlecap.create(drink: "Summer Shandy", brand: "Jacob Leinenkugel Brewing Company", parent: "Anheuser-Busch InBev", favorite: false, cap_detail_list: "light blue, lemon", pinned: false)
 Bottlecap.create(drink: "Unspecified", brand: "Seagram's", parent: "Diageo", favorite: false, cap_detail_list: "blue, palm trees", pinned: false)
 Bottlecap.create(drink: "Session Black Lager", brand: "Full Sail Brewing Company", parent: "Full Sail Brewing Company", favorite: false, cap_detail_list: "dark blue, dark gray", pinned: false)
-Bottlecap.create(drink: "Old Speckled Hen", brand: "Morland Brewery", parent: "Greene King Brewery", favorite: false, cap_detail_list: "red, gold, Morland Brewery", pinned: false)
-Bottlecap.create(drink: "Unspecified", brand: "Woodchuck Hard Cider", parent: "Vermont Hard Cider Company", favorite: false, cap_detail_list: "black", pinned: false)
+Bottlecap.create(drink: "Old Speckled Hen", brand: "Morland Brewery", parent: "Greene King Brewery", favorite: false, cap_detail_list: "red, gold", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Woodchuck Hard Cider", parent: "Vermont Hard Cider Company", favorite: false, cap_detail_list: "black, white", pinned: false)
 Bottlecap.create(drink: "Pale 31", brand: "Firestone Walker Brewing Company", parent: "Duvel Moortgat Brewery", favorite: false, cap_detail_list: "yellow, California", pinned: false)
-Bottlecap.create(drink: "Unspecified", brand: "Ninkasi Brewing Company", parent: "Ninkasi Brewing Company", favorite: false, cap_detail_list: "black, blue, teal, N", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Ninkasi Brewing Company", parent: "Ninkasi Brewing Company", favorite: false, cap_detail_list: "black, blue, teal, big letter", pinned: false)
 Bottlecap.create(drink: "Summer Ale", brand: "Samuel Adams", parent: "Boston Brewing Company", favorite: false, cap_detail_list: "yellow, orange", pinned: false)
-Bottlecap.create(drink: "Unspecified", brand: "Squatter's Craft Beer", parent: "Utah Brewers Collective", favorite: false, cap_detail_list: "cream, beige, silver", pinned: false)
-Bottlecap.create(drink: "Omission IPA", brand: "Omission Beer", parent: "Omission Beer", favorite: false, cap_detail_list: "green, black, O", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Squatters Craft Beer", parent: "Utah Brewers Cooperative", favorite: false, cap_detail_list: "beige, silver, circle, 1989", pinned: false)
+Bottlecap.create(drink: "IPA", brand: "Omission Beer", parent: "Omission Beer", favorite: false, cap_detail_list: "green, black, big letter", pinned: false)
 Bottlecap.create(drink: "Michelob Ultra Lime Cactus", brand: "Michelob", parent: "Anheuser-Busch InBev", favorite: false, cap_detail_list: "white, green", pinned: false)
-Bottlecap.create(drink: "Harvest Patch Shandy", brand: "Jacob Leinenkugel Brewing Company", parent: "SABMiller", favorite: false, cap_detail_list: "white, pumpkin", pinned: false)
+Bottlecap.create(drink: "Harvest Patch Shandy", brand: "Jacob Leinenkugel Brewing Company", parent: "Anheuser-Busch InBev", favorite: false, cap_detail_list: "orange, white, pumpkin", pinned: false)
 Bottlecap.create(drink: "Smith & Forge Hard Cider", brand: "Smith & Forge", parent: "MillerCoors", favorite: false, cap_detail_list: "orange, black, hammer", pinned: false)
 Bottlecap.create(drink: "Unspecified", brand: "New Planet Beer Company", parent: "New Planet Brewery", favorite: false, cap_detail_list: "green, white", pinned: false)
 Bottlecap.create(drink: "Revive Rootbeer", brand: "LIVE Soda", parent: "LIVE Soda", favorite: false, cap_detail_list: "brown, white, red, live", pinned: false)
-Bottlecap.create(drink: "Oktoberfest", brand: "Jacob Leinenkugel Brewing Company", parent: "SABMiller", favorite: false, cap_detail_list: "blue, white, checkerboard", pinned: false)
-Bottlecap.create(drink: "Best Damn Root Beer", brand: "Best Damn Brewing Company", parent: "Anheuser-Busch InBev", favorite: false, cap_detail_list: "brown, white, black", pinned: false)
+Bottlecap.create(drink: "Oktoberfest", brand: "Jacob Leinenkugel Brewing Company", parent: "Anheuser-Busch InBev", favorite: false, cap_detail_list: "blue, white, checkerboard", pinned: false)
+Bottlecap.create(drink: "Root Beer", brand: "Best Damn Brewing Company", parent: "Anheuser-Busch InBev", favorite: false, cap_detail_list: "brown, white, black", pinned: false)
 Bottlecap.create(drink: "Spaten Lager", brand: "Spaten", parent: "Anheuser-Busch InBev", favorite: false, cap_detail_list: "white, shovel, red", pinned: false)
 Bottlecap.create(drink: "IPA", brand: "Odell Brewing Company", parent: "Odell Brewing Company", favorite: false, cap_detail_list: "orange, white, leaf, hops", pinned: false)
 Bottlecap.create(drink: "Draft Cider", brand: "Newton's Folly", parent: "Vermont Hard Cider Company", favorite: false, cap_detail_list: "black, white, apple", pinned: false)
@@ -43,7 +40,22 @@ Bottlecap.create(drink: "Longboard Island Ale", brand: "Kona Brewing Company", p
 Bottlecap.create(drink: "Unspecified", brand: "Unknown", parent: "Unknown", favorite: false, cap_detail_list: "gold", pinned: false)
 Bottlecap.create(drink: "Unspecified", brand: "Shock Top", parent: "Anheuser-Busch InBev", favorite: false, cap_detail_list: "red, maroon, wheat", pinned: false)
 Bottlecap.create(drink: "Unspecified", brand: "Maredsous", parent: "Duvel Moortgat Brewery", favorite: false, cap_detail_list: "blue, gold, hexagon", pinned: false)
-Bottlecap.create(drink: "", brand: "", parent: "", favorite: false, cap_detail_list: "", pinned: false)
+Bottlecap.create(drink: "Southern Peach Ginger Ale", brand: "Fest Cola", parent: "Fest Cola", favorite: false, cap_detail_list: "white, silver, circle, Flavor is our Birthright", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Leibinger Brewery", parent: "Leibinger Brewery", favorite: false, cap_detail_list: "white, red, black, crown, crest", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Firestone Walker Brewing Company", parent: "Duvel Moortgat Brewery", favorite: false, cap_detail_list: "black, gold, fighting, bear", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Unknown", parent: "Unknown", favorite: false, cap_detail_list: "black", pinned: false)
+Bottlecap.create(drink: "LandShark Lager", brand: "Margaritaville Brewing Company", parent: "Anheuser-Busch InBev", favorite: false, cap_detail_list: "yellow, blue, shark, fin, palm trees", pinned: false)
+Bottlecap.create(drink: "Strawberry Ale", brand: "Redd's", parent: "Anheuser-Busch InBev", favorite: false, cap_detail_list: "green, black, gold, shield, big letter", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Saranac Brewery", parent: "Matt Brewing Company", favorite: false, cap_detail_list: "gold, yellow, 1888, circle, white", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Stone Brewing", parent: "Stone Brewing", favorite: false, cap_detail_list: "gold, black, devil, 20, anniversary", pinned: false)
+Bottlecap.create(drink: "Sparkling Apple", brand: "Izze", parent: "", favorite: false, cap_detail_list: "light green, silver, flower", pinned: false)
+Bottlecap.create(drink: "Blonde", brand: "Affligem Brewery", parent: "Heineken", favorite: false, cap_detail_list: "beige, black, red, shield", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Lindemans Brewery", parent: "Lindemans Brewery", favorite: false, cap_detail_list: "gold, brown, anno, 1822, oversized", pinned: false)
+Bottlecap.create(drink: "Cherry Cola", brand: "Best Damn Brewing Company", parent: "Anheuser-Busch InBev", favorite: false, cap_detail_list: "maroon, white, black, circle", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Ceramico Alfredo Gioventù Portofino", parent: "Ceramico Alfredo Gioventù Portofino", favorite: false, cap_detail_list: "white, yellow, red, Jesus, fish", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Woodchuck Hard Cider", parent: "Vermont Hard Cider Company", favorite: false, cap_detail_list: "black, white", pinned: false)
+Bottlecap.create(drink: "Unspecified", brand: "Squatters Craft Beer", parent: "Utah Brewers Cooperative", favorite: false, cap_detail_list: "beige, silver", pinned: false)
+# Bottlecap.create(drink: "", brand: "", parent: "", favorite: false, cap_detail_list: "", pinned: false)
 # Bottlecap.create(drink: "", brand: "", parent: "", favorite: false, cap_detail_list: "", pinned: false)
 # Bottlecap.create(drink: "", brand: "", parent: "", favorite: false, cap_detail_list: "", pinned: false)
 # Bottlecap.create(drink: "", brand: "", parent: "", favorite: false, cap_detail_list: "", pinned: false)
