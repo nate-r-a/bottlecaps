f9cf278dee956633bf1628d3d0b4fcc57c435b97
Nate Rudnick Aldridge <nr265@cornell.edu>
2017-03-22 02:47:21 +0000

    Added more caps

--- app/models/bottlecap.rb
+++ app/models/bottlecap.rb
@@ -5,4 +5,9 @@ class Bottlecap < ActiveRecord::Base
   before_create do
     self.drink_detail_list.add(self.drink, self.brand, self.parent)
   end
+  
+  def is_complete?
+    [self.brand, self.parent].include?(nil) ? false : true
+  end
+  
 end
